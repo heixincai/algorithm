c9fa2372fc3373046daa85fddafc4c9b41373a5b
krosshuang <670283005@qq.com>
2018-12-14 15:35:16 +0800

    Add comments

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -2,16 +2,19 @@ package search;
 
 public class BinarySearch {
 
+    // 测试基准，查找 10000000次，在长度为100的数组中找第一个元素
+    // 右移比除以2快19毫秒
+    // final比没有final快5毫秒
+    // 逻辑右移比算术右移快2毫秒
+
     public static int search(int[] arr, int target) {
         int lo = 0;
         int hi = arr.length - 1;
 
         while (lo <= hi) {
-            // 右移比除以2快19毫秒
-            // final比没有final快5毫秒
-            // 逻辑右移比算术右移快2毫秒
             final int mid = (lo + hi) >>> 1;
             final int value = arr[mid];
+
             if (value == target) {
                 return mid;
             } else if (value > target) {
