ec93ae9c6b5d37d8db84b30fc29065f88d7ae2ac
kross <670283005@qq.com>
2019-03-26 01:20:38 +0800

    commit leetcode

---
+++ src/util/ConvertCopyTextToClassName.java
@@ -0,0 +1,13 @@
+package util;
+
+/**
+ * Created by krosshuang on 2019/3/26.
+ */
+public class ConvertCopyTextToClassName {
+
+    public static void main(String[] s) {
+        String problem = "938. Range Sum of BST";
+
+        System.out.println("P" + problem.replace(".", "").replace(" ", "_"));
+    }
+}
