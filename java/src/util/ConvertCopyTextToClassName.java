17780d216f0e64db889477d1f112571c28e4d7f8
kross <670283005@qq.com>
2019-05-16 09:27:35 +0800

    commit

--- java/src/util/ConvertCopyTextToClassName.java
+++ java/src/util/ConvertCopyTextToClassName.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class ConvertCopyTextToClassName {
 
     public static void main(String[] s) {
+        System.out.print("Plz input question name:");
         Scanner scanner = new Scanner(System.in);
         String problem = scanner.nextLine();
         //String problem = "107. Binary Tree Level Order Traversal II";
