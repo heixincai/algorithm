83a7f78c6b87c5871b3d2a528c775aab9870551d
krosshuang <670283005@qq.com>
2019-08-23 21:50:08 +0800

    测试volatile

--- java/src/test/TestVolatile.java
+++ java/src/test/TestVolatile.java
@@ -2,15 +2,21 @@ package test;
 
 public class TestVolatile {
 
-    public int a1 = 0;
-    public volatile int a2 = 0;
     public static int a3 = 0;
 
-    public static int count = 1000000;
+    public static int count = 1000000000;
 
     public static int examCount = 10;
 
     public static void main(String[] arg) {
+        test1();
+        //test2();
+    }
+
+    /**
+     * 测试volatile关键字对读写性能的影响
+     * */
+    public static void test1() {
         TestVolatile test = new TestVolatile();
 
         long a1Count = 0;
@@ -22,7 +28,8 @@ public class TestVolatile {
             long cost = 0;
 
             for (int i = 0; i < count; i++) {
-                test.a1++;
+                int v = test.a1;
+                v++;
             }
 
             cost = (System.currentTimeMillis() - t);
@@ -33,7 +40,8 @@ public class TestVolatile {
             t = System.currentTimeMillis();
 
             for (int i = 0; i < count; i++) {
-                test.a2++;
+                int v = test.a2++;
+                v++;
             }
 
             cost = (System.currentTimeMillis() - t);
@@ -57,7 +65,55 @@ public class TestVolatile {
         System.out.println("" + 1f * a1Count / examCount);
         System.out.println("" + 1f * a2Count / examCount);
         System.out.println("" + 1f * a3Count / examCount);
+    }
 
+    public int a1 = 0;
+    public volatile int a2 = 0;
 
+    /**
+     * 测试volatile是否具备原子性
+     * */
+    public static void test2() {
+
+        TestVolatile tv = new TestVolatile();
+
+        int count = 8000;
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < count; i++) {
+                    tv.a1++;
+                }
+                for (int i = 0; i < count; i++) {
+                    tv.a2++;
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < count; i++) {
+                    tv.a1++;
+                }
+                for (int i = 0; i < count; i++) {
+                    tv.a2++;
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (Throwable e) {
+
+        }
+
+        System.out.println(tv.a1);
+        System.out.println(tv.a2);
     }
 }
